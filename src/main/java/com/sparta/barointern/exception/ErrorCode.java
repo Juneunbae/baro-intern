ab7227393ce07e5868c5d8ee628eddf75319db62
juneunbae <dmsqo1403@naver.com>
2025-07-26 13:37:19 +0900

    fix: Exception 수정
    GlobalExceptionHandler.java
    - handleAlreadyExists -> handleGlobalException 수정
    
    ErrorCode.java
    - USERNAME_LENGTH_5_NOT_OVER 에러 메시지 추가
    - INVALID_INPUT_VALUE 에러 메시지 추가

--- src/main/java/com/sparta/barointern/exception/ErrorCode.java
+++ src/main/java/com/sparta/barointern/exception/ErrorCode.java
@@ -1,13 +1,19 @@
 package com.sparta.barointern.exception;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-	ALREADY_EXISTS("ALREADY_EXISTS", "이미 존재하는 사용자입니다.");
+	ALREADY_EXISTS("ALREADY_EXISTS", "이미 존재하는 사용자입니다.", HttpStatus.CONFLICT),
+	USERNAME_LENGTH_5_NOT_OVER("USERNAME_LENGTH_5_NOT_OVER", "유저 아이디가 5글자 미만입니다.", HttpStatus.BAD_REQUEST),
+	INVALID_INPUT_VALUE("INVALID_INPUT_VALUE", "필수 입력 값이 누락되었거나 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
+	;
 
 	private final String code;
 	private final String message;
+	private final HttpStatus httpStatus;
 }
--- src/main/java/com/sparta/barointern/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/barointern/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.sparta.barointern.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,10 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-	@ExceptionHandler(GlobalException.class)
-	public ResponseEntity<ErrorResponseDto> handleAlreadyExists(ErrorCode e) {
-		ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getCode(), e.getMessage());
 
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponseDto);
+	@ExceptionHandler(GlobalException.class)
+	public ResponseEntity<ErrorResponseDto> handleGlobalException(GlobalException ex) {
+		ErrorCode errorCode = ex.getErrorCode();
+
+		ErrorResponseDto errorResponseDto = new ErrorResponseDto(errorCode.getCode(), errorCode.getMessage());
+		return ResponseEntity.status(errorCode.getHttpStatus()).body(errorResponseDto);
 	}
 }
